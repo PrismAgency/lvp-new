bdf50134cf26318252fb1cef5f83a1f890c860ce
julien Grelet <juliengrelet@MBPdeCharles2.wifi.froment.in.iesanetwork.com>
2016-12-02 17:41:40 +0100

    modif js home

--- home.php
+++ home.php
@@ -11,6 +11,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Style CSS -->
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -20,6 +21,25 @@
     <![endif]-->
   </head>
   <body>
+
+    <!-- preloader -->
+    <div id="preloader">
+      <div id="content-intro">
+        <h1 class="tlt">
+          <ul class="texts">
+            <li data-in-effect="fadeIn">VOUS PENSEZ ETRE LIBRE</li>
+            <li data-in-effect="fadeIn">VOUS VOUS TROMPEZ</li>
+          </ul>
+        </h1>
+        <span id="title-agence">#lavaguepourpre</span>
+        <!-- /.tlt -->
+      </div>
+      <div class="footer-preloader">
+        <button class="buttonSkipe">skip intro</button>
+      </div>
+    </div>
+    <!-- /.preloader -->
+
     <header>
       <nav class="navbar navbar-default">
         <div class="container-fluid">
@@ -48,6 +68,10 @@
     <script src="js/jQuery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <!-- custom javascript -->
+    <script src="js/jquery.lettering.js"></script>
+    <!-- custom javascript -->
+    <script src="js/jquery.textillate.js"></script>
     <!-- custom javascript -->
     <script src="js/custom.js"></script>
 
--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,17 @@
 // feuille javascript
+$(document).ready(function(){  
+
+	// preloader site
+	$(window).on('load', function() { // makes sure the whole site is loaded 
+  		$('#preloader').delay(10000).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  		$('body').delay(7000).css({'overflow':'visible',});
+  		$(".buttonSkipe").click(function(){ 
+    		$("#preloader").remove();
+  		});
+	});
+
+	$(function () {
+    	$('.tlt').textillate();
+	});
+
+});
