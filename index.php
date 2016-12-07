b3d68b5bc350c34d78d6952d3c79f32b2ceec4c9
julien Grelet <juliengrelet@iPhone-de-Chen.wifi.froment.in.iesanetwork.com>
2016-12-07 10:53:26 +0100

    modif bg, footer, header

--- index.php
+++ index.php
@@ -1,143 +1,3 @@
-<<<<<<< HEAD
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>La vague pourpre</title>
-
-    <!-- fichier css -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/jquery.fullPage.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-  </head>
-  <body>
-
-    <!-- preloader -->
-    <div id="preloader">
-      <div id="content-intro">
-        <h1 class="tlt">
-          <ul class="texts">
-            <li data-in-effect="fadeIn">Vous pensez être Libre</li>
-            <li data-in-effect="fadeIn">Vous vous trompez</li>
-          </ul>
-        </h1>
-        <span id="title-agence">#lavaguepourpre</span>
-        <!-- /.tlt -->
-      </div>
-      <div class="footer-preloader">
-        <button class="buttonClose">Passer</button>
-      </div>
-    </div>
-    <!-- /.preloader -->
-
-    <!-- header -->
-    <header id="header-principal">
-      <nav class="navbar">
-        <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <h1 id="logo"><a class="navbar-brand" href="index.php"><span class="icon-logo"></span></a></h1>
-          </div>
-
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Adhérer</a></li>
-              <li><a href="#">Actualités</a></li>
-            </ul>
-          </div><!-- /.navbar-collapse -->
-        
-        </div><!-- /.container-fluid -->
-      </nav>
-    </header><!-- /.header -->
-
-    <div id="fullpage"><!-- full page -->
-      <div class="section" id="section1"><!-- section -->
-   
-        <div class="slide" id="slide1" data-anchor="slide1">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-6">
-                <h1>Vous pensez être libre ?</h1>
-              </div>
-              <div class="col-lg-6">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="slide" id="slide2" data-anchor="slide2">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8 col-lg-offset-2">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div><!-- /.section -->
-    </div><!-- /.full page -->
-
-    <!-- footer principal -->
-    <footer id="footer-principal">
-      <div class="container-fluid">
-        <div class="row">
-
-          <div id="social-media" class="col-lg-6 col-md-6 col-sm-12">
-            <p><button>Social</button></p>
-          </div>
-
-          <div id="copyright" class="col-lg-6 col-md-6 col-sm-12">
-            <p>La Vague Pourpre - 2016 - <a href="#">mentions légales</a></p>
-          </div>
-
-        </div>
-      </div>
-    </footer>
-    <!-- /.footer principal -->
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jQuery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/edit/jquery.lettering.js"></script>
-    <script src="js/edit/jquery.textillate.js"></script>
-    <script src="js/jquery.fullPage.min.js"></script>
-    <script src="js/custom.js"></script>
-
-  </body>
-</html>
-=======
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -206,8 +66,7 @@
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Adhérer</a></li>
-              <li><a href="#">Actualités</a></li>
+              <li><a href="actualites.php">Actualités</a></li>
             </ul>
           </div><!-- /.navbar-collapse -->
         
@@ -221,6 +80,19 @@
         <div class="slide" id="slide1" data-anchor="slide1">
           <div class="container">
             <div class="row">
+              <div class="row-eq-height col-lg-8 col-lg8 col-sm-12">
+                <h1>êtes vous rééllement<span class="border-title"></span> libre ?</h1>
+              </div>
+              <div class="row-eq-height col-lg-4 col-lg-4 col-sm-12">
+                <p><a href="#firstPage/slide2" class="btn-1">Pourquoi ?</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="slide" id="slide2" data-anchor="slide2">
+          <div class="container">
+            <div class="row">
               <div class="col-lg-8 col-lg-offset-2">
                 <div class="block-4 pst-absolute">
                     <div class="Oval-2 pst-relative">
@@ -228,14 +100,14 @@
                             <br /><span class="en-sont-dpendant">en sont<br />dépendant</span>
                         </p>
                     </div>
-                    <div class="Rectangle-5 fp-next pst-relative"><p class="right-arrow-of-straight-lines">next slide 2</p></div>
+                    <div class="Rectangle-5 fp-next pst-relative"><p class="right-arrow-of-straight-lines"><a href="#slide3">next slide 2</a></p></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="slide" id="slide2" data-anchor="slide2">
+        <div class="slide" id="slide3" data-anchor="slide3">
           <div class="container">
             <div class="row">
               <div class="col-lg-8 col-lg-offset-2">
@@ -252,7 +124,7 @@
           </div>
         </div>
           
-        <div class="slide" id="slide3" data-anchor="slide3">
+        <div class="slide" id="slide4" data-anchor="slide4">
           <div class="container">
             <div class="row">
               <div class="col-lg-8 col-lg-offset-2">
@@ -267,9 +139,26 @@
               </div>
             </div>
           </div>
-        </div>  
-          
-       <div class="slide" id="slide4" data-anchor="slide4">
+        </div>    
+
+        <div class="slide" id="slide5" data-anchor="slide5">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 col-lg-offset-2">
+                <div class="block-4 pst-absolute">
+                    <div class="Oval-2 pst-relative">
+                        <p class="text-1-Oval-2 pst-relative"><span class="slide1-p-layer">3 personnes / 4</span>
+                            <br /><span class="en-sont-dpendant">en sont<br />dépendant</span>
+                        </p>
+                    </div>
+                    <div class="Rectangle-5 fp-next pst-relative"><p class="right-arrow-of-straight-lines">next slide 2</p></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="slide" id="slide6" data-anchor="slide6">
           <div class="container">
             <div class="row">
               <div class="col-lg-8 col-lg-offset-2">
@@ -282,7 +171,17 @@
               </div>
             </div>
           </div>
-        </div>   
+        </div>
+
+        <div class="slide" id="slide7" data-anchor="slide7">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <h1 class="title-important">Alors ?<br>vous avez deviné ?</h1>
+              </div>
+            </div>
+          </div>
+        </div> 
           
       </div><!-- /.section -->
     </div><!-- /.full page -->
@@ -292,21 +191,24 @@
       <div class="container-fluid">
         <div class="row">
 
-            <div id="social-media" class="col-lg-3 col-md-3 col-sm-12">
-                <p id="share" class="s-H-A-R-E">S<br />H<br />A<br />R<br />E</p>
-              <ul id="rs">
-		 <li id="logo-facebook"> 
-		    <a href="https://www.facebook.com/LaVaguePourpre/?fref=ts" target="_blank" id="facebook">facebook</a>
-		 </li>
-		 <li id="logo-twitter">
-		    <a href="https://twitter.com/LaVaguePourpre" target="_blank" id="twitter">twitter</a>
-		 </li>
-              </ul>
+          <!-- social média -->
+          <div id="social-media" class="col-lg-6 col-md-6 col-sm-12">
+            <a>Share</a>
+            <p id="reseaux-soc">
+
+              <a href="https://www.facebook.com/Johanna-Bros-1732667030323472/?fref=ts" target="_blank"><i data-toggle="tooltip" data-placement="top" title="Facebook" class="fa fa-facebook" aria-hidden="true"></i></a>
+
+              <a href="https://www.instagram.com/johannabros/" target="_blank"><i data-toggle="tooltip" data-placement="top" title="twitter" class="fa fa-twitter" aria-hidden="true"></i></a>
+
+            </p>
           </div>
-          <div class="col-lg-4 col-md-4 col-sm-12"></div>
-          <div id="copyright" class="col-lg-3 col-md-3 col-sm-12">
+          <!-- /.social média -->
+
+          <!-- copyright -->
+          <div id="copyright" class="col-lg-6 col-md-6 col-sm-12">
             <p>La Vague Pourpre - 2016 - <a href="#">mentions légales</a></p>
           </div>
+          <!-- /.copyright -->
 
         </div>
       </div>
--- js/custom.js
+++ js/custom.js
@@ -17,8 +17,7 @@ $(document).ready(function(){
 
 	$('#fullpage').fullpage({
 		anchors: ['firstPage', 'secondPage', '3rdPage'],
-		sectionsColor: ['#00000', '#00000', '#00000'],
-		slidesNavigation: true,
+		sectionsColor: ['#000000', '#000000', '#000000'],
 	});
 
 });
