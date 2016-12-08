adc41958e9326d22142bad41a8b51e7c2edb6f91
julien Grelet <juliengrelet@SudeMacBook-Pro.admin.froment.in.iesanetwork.com>
2016-12-08 18:05:04 +0100

    modif video, add video

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<?php if(isset($_SERVER['SCRIPT_NAME']) && $_SERVER['SCRIPT_NAME'] == '/actualites.php'){ ?>    
+<?php if(isset($_SERVER['SCRIPT_NAME']) && $_SERVER['SCRIPT_NAME'] == '/lvp-new/actualites.php'){ ?>    
    <!-- header -->
     <header id="header-principal">
       <nav class="navbar">
@@ -17,7 +17,7 @@
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="/home">Home</a></li>
+              <li><a href="/lvp-new/index.php">Home</a></li>
             </ul>
           </div><!-- /.navbar-collapse -->
         
@@ -37,13 +37,13 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <h1 id="logo"><a class="navbar-brand" href="index.php"><span class="icon-logo"></span></a></h1>
+            <h1 id="logo"><a class="navbar-brand" href="/lvp-new/index.php"><span class="icon-logo"></span></a></h1>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="actualites">Actualités</a></li>
+              <li><a href="/lvp-new/actualites.php">Actualités</a></li>
             </ul>
           </div><!-- /.navbar-collapse -->
         
