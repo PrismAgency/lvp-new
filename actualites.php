eb9c22f42a7a6ced2912ceeb75394bdc202f0189
admin <admin@meli-melo>
2016-12-09 08:31:11 +0100

    modif 404 responsive

--- actualites.php
+++ actualites.php
@@ -1,5 +1,3 @@
-
-
     <!-- header -->
     <?php include 'includes/header.php'; ?>
 
@@ -8,7 +6,7 @@
         <div class="row">
           <div class="col-lg-12">
             <h1 class="text-center">Actualités</h1>
-            <h2 class="text-center">Découvrez toutes l'actualités de La vague pourpre</h2>
+            <h2 class="text-center">Découvrez toute l'actualité de La Vague Pourpre</h2>
           </div>
         </div>
       </div>
@@ -17,15 +15,15 @@
     <main class="section-padding">
       <div class="container">
         <div class="row">
-          <article class="col-lg-4">
+          <article class="col-lg-6">
             <section>
 		<p><a class="twitter-timeline" data-height="600" data-dnt="true" data-theme="light" data-link-color="#2B94D6" href="https://twitter.com/LaVaguePourpre">Tweets by LaVaguePourpre</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></p>
               </section>
           </article>
 	    
-	  <article class="col-lg-4">
+	  <article class="col-lg-6">
 	      <section>
-	      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLaVaguePourpre%2F&tabs=timeline&width=340&height=700&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="340" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+	      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLaVaguePourpre%2F&tabs=timeline&width=555&height=700&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" data-dnt="true" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 	      </section>
           </article>  
         </div>
--- errors/404.php
+++ errors/404.php
@@ -3,13 +3,18 @@
     <div id="block-404">
 	<div class="container">
 	    <div class="row">
-		<div class="logo-404 col-lg-4">
-			<img src="../images/logo.png">
+		<div class=" col-lg-6 col-md-6 col-sm-6 col-xs-12">
+		    <div class="block-logo-404">
+			<img class="logo-404" src="../images/logo.png">
+		    </div>
 		</div>
-		<div id="text-404" class="col-lg-8">
-			<h1>404</h1>	
-			<p>Erreur, page introuvable.</p>
-			<p>Veuillez cliquer sur la <a href="home">home</a></p>
+		<div id="text-404" class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+		    <div>
+			<h2>Ooops<span>!</span></h2>
+			<h1>4<span>0</span>4</h1>	
+			<p>La page que vous cherchez n'existe pas</p>
+			<p><a href="home">Retournez à la page d'accueil</a></p>
+		    </div>
 		</div>
 	    </div>
 	</div>
